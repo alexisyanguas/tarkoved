5ae676f04270142026ac2e90fc3bba8a4a33233d
Alexis Yanguas <yanguasalexis@gmail.com>
2024-01-18 13:17:19 +0100

    🚀 BUILD

---
+++ public/build/assets/Welcome-0fd1dc21.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-757fa5df.js";import{L as r}from"./Layout-88e6faf6.js";function p({layoutDatas:t}){return e(r,{title:t.title,page:t.page})}export{p as default};
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,16 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>
-        {{ config('app.name', 'Tarkoved') }}
-    </title>
-
-    <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+
+    <title>{{ config('app.name', 'Tarkoved') }}</title>
     @inertiaHead
+
+    <link rel="stylesheet" href="/build/assets/app-00c87a7b.css">
+    <script src="/build/assets/app-757fa5df.js" type="module"></script>
+
+    {{-- @viteReactRefresh --}}
+    {{-- @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) --}}
 </head>
 
 <body>
