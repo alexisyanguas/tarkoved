4c91cf922902cc407de635e01ee70100eaa7f230
Alexis Yanguas <yanguasalexis@gmail.com>
2024-01-18 15:47:43 +0100

    🚀 BUILD

---
+++ public/build/assets/Welcome-0a836fb0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-5717204d.js";import{L as r}from"./Layout-c1b14d81.js";function p({layoutDatas:t}){return e(r,{title:t.title,page:t.page})}export{p as default};
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,10 +9,13 @@
         {{ config('app.name', 'Tarkoved') }}
     </title>
 
+    <link href="/build/assets/app-1319725d.css" rel="stylesheet">
+    <script src="/build/assets/app-5717204d.js" type="module"></script>
+
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    {{-- @viteReactRefresh --}}
+    {{-- @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) --}}
     @inertiaHead
 </head>
 
