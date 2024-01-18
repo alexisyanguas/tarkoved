74d67fe66341ca48e4db6f1b2420d3f4a5d07a83
Alexis Yanguas <yanguasalexis@gmail.com>
2024-01-18 15:39:36 +0100

    🚀 BUILD

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,15 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/build/assets/app-1319725d.css">
+    <script src="/build/assets/app-5717204d.js" type="module"></script>
 
-    <title>{{ config('app.name', 'Tarkoved') }}</title>
     @inertiaHead
-
-    <link rel="stylesheet" href="/build/assets/app-00c87a7b.css">
-    <script src="/build/assets/app-757fa5df.js" type="module"></script>
-
-    {{-- @viteReactRefresh --}}
-    {{-- @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) --}}
 </head>
 
 <body>
