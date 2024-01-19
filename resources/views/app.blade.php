7855ebb1e5f17612153d3a15600508267eaefd76
Alexis Yanguas <yanguasalexis@gmail.com>
2024-01-19 10:23:29 +0100

    🔨 Remove public_html

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,13 @@
     <title>
         {{ config('app.name', 'Tarkoved') }}
     </title>
-    <link href="/build/assets/app-3dbfa2ed.css" rel="stylesheet">
-    <script src="/build/assets/app-79645224.js" type="module"></script>
+    {{-- <link href="/build/assets/app-3dbfa2ed.css" rel="stylesheet"> --}}
+    {{-- <script src="/build/assets/app-79645224.js" type="module"></script> --}}
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    {{-- @viteReactRefresh --}}
-    {{-- @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) --}}
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
 </head>
 
